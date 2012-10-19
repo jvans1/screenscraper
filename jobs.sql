d85adde968b347a0cca63786515c00c85669b5ca
James Vanneman <jvannem@gmail.com>
2012-10-18 20:42:16 -0400

    working version

---
+++ jobs.sql
@@ -0,0 +1,6 @@
+CREATE TABLE jobs (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	description TEXT,	
+	url TEXT
+);
--- rubyjobscraper.rb
+++ rubyjobscraper.rb
@@ -8,48 +8,52 @@
 		##About the Company
 
 		##REPEAT
-
-
-
 require 'nokogiri'
 require 'rubygems'
 require 'open-uri'
+require 'sqlite3'
 
-
-url = "http://careers.stackoverflow.com"
+@db = SQLite3::Database.new("jobs.db")
+@db.execute("CREATE TABLE jobs (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	description TEXT,	
+	url TEXT
+);")
+url= "http://careers.stackoverflow.com"
+#handle inserts
+def insert_job(title, description, url)
+	@db.execute("INSERT INTO jobs(title, description, url)
+			VALUES(?, ?, ?)", title, description, url)
+end
 # Open individual jobs' pages
-
-
-def parse_jobs(links, url = "http://careers.stackoverflow.com")
+def parse_jobs(links, url)
 	links.each do |link|
 		job_page = Nokogiri::HTML(open(url+link))
 	#Parse the page for contents
 		job_title = job_page.css('h1#title').text
-		job_array = job_page.css('div.description p').text.split("</p>")[0]
-
-
+		job_description = job_page.css('div.description p').text
 		job_url = url + link
-		raise job_array.inspect
+		# puts job_title, job_url
+		insert_job(job_title, job_description, job_url)
 		end
 end
-#select all tabs
-#open a different page
 
+puts "What type of job do you want to search for?"
+job = gets.chomp
+search_page = "http://careers.stackoverflow.com/jobs?searchTerm=#{job}&location=new+york+city"
 
-#puts "What do you want to search"
-#Open Job search page
-search_page = "http://careers.stackoverflow.com/jobs?searchTerm=ruby&location=new+york+city"
 search_results = Nokogiri::HTML(open(search_page))
 
 #Put links to job pages into an Array
-first_page = search_results.css('a.title').map { |n| n['href'] }
+jobs_array = search_results.css('a.title').map { |n| n['href'] }
 
-parse_jobs(first_page)
+parse_jobs(jobs_array, url)
 
 # Open the next page from index page and assign it to current page 
 
 next_page_extension = search_results.css('div.pagination a.prev-next')[0]['href']
 next_page = Nokogiri::HTML(open(url+next_page_extension))
 next_page_links = search_results.css('a.title').map { |n| n['href'] }
-parse_jobs(next_page_links)
+parse_jobs(next_page_links, url)
 
